9d5d280f72ad07c55252610a2bac3a1ac52aaf27
oraqlle <41113853+oraqlle@users.noreply.github.com>
2022-08-04 16:35:07 +1000

    Created a generic data type called `lval`. This allows data to either be valid or an error value. Added corresponding enums for error and data states that are used to diagnose and handle errors. Refactored main functions to utilise `lval`'s.

--- src/parsing.main.c
+++ src/parsing.main.c
@@ -25,32 +25,98 @@
     #include <editline/history.h>
 #endif
 
-long eval_op(long x, char* op, long y)
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
+
+enum { LVAL_NUM, LVAL_ERR };  ///< type
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };  ///< error types
+
+lval lval_num(long x)
 {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
+}
+
+lval lval_err(int x)
+{
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+lval eval_op(lval x, char* op, lval y)
+{
+    if (x.type == LVAL_ERR)
+        return x;
+    if (y.type == LVAL_ERR)
+        return y;
+
     if (strcmp(op, "+") == 0)
-        return x + y;
+        return lval_num(x.num + y.num);
     
     if (strcmp(op, "-") == 0)
-        return x - y;
+        return lval_num(x.num - y.num);
     
     if (strcmp(op, "*") == 0)
-        return x * y;
+        return lval_num(x.num * y.num);
     
     if (strcmp(op, "/") == 0)
-        return x / y;
-    
-    return 0;
+    {
+        return y.num == 0 
+            ? lval_err(LERR_DIV_ZERO) 
+            : lval_num(x.num / y.num);
+    }
+
+    return lval_err(LERR_BAD_OP);
+}
+
+void lval_print(lval v)
+{
+    switch (v.type)
+    {
+        case LVAL_NUM:
+            printf("%li", v.num);
+            break;
+
+        case LVAL_ERR:
+            if (v.err == LERR_DIV_ZERO)
+                printf("Error: Division by zero.");
+
+            if (v.err == LERR_BAD_OP)
+                printf("Error: Invalid operator.");
+
+            if (v.err == LERR_BAD_NUM)
+                printf("Error: Invalid number.");
+
+            break;
+    }
+}
+
+void lval_println(lval v)
+{
+    lval_print(v);
+    putchar('\n');
 }
 
 
-long eval(mpc_ast_t* t)
+lval eval(mpc_ast_t* t)
 {
     if (strstr(t->tag, "number"))
-        return atoi(t->contents);
+    {
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
+    }
 
     char* op = t->children[1]->contents;
 
-    long x = eval(t->children[2]);
+    lval x = eval(t->children[2]);
 
     int i = 3;
     while (strstr(t->children[i]->tag, "expr"))
@@ -94,8 +160,8 @@ int main(int argc, char** argv)
 
         if (mpc_parse("<stdin>", input, Curly, &r))
         {
-            long result = eval(r.output);
-            printf("%li\n", result);
+            lval result = eval(r.output);
+            lval_println(result);
             mpc_ast_delete(r.output);
         }
         else
