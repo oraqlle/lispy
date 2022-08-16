3d3b5565794a2deb3430f57ea9073e12e78a49b5
oraqlle <41113853+oraqlle@users.noreply.github.com>
2022-08-16 16:10:33 +1000

    Fixed the parser grammer

--- src/curly.main.c
+++ src/curly.main.c
@@ -16,7 +16,7 @@ int main()
     mpca_lang(MPCA_LANG_DEFAULT,
         "                                                               \
             number      : /-?[0-9]+/ ;                                  \
-            symbol      : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+ ;             \
+            symbol      : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/ ;             \
             sexpr       : '(' <expr>* ')' ;                             \
             qexpr       : '{' <expr>* '}' ;                             \
             expr        : <number> | <symbol> | <sexpr> | <qexpr> ;     \
